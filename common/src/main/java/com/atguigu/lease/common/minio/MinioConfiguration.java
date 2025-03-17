e2e593719632dd5d1ab06b10399dcd6549033a8a
waq666 <qwe714509665@qq.com>
2025-03-17 20:20:15 +0800

    minio上传文件

---
+++ common/src/main/java/com/atguigu/lease/common/minio/MinioConfiguration.java
@@ -0,0 +1,22 @@
+package com.atguigu.lease.common.minio;
+
+import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationPropertiesScan("com.atguigu.lease.common.minio")
+public class MinioConfiguration {
+    @Autowired
+    private MinioProperties minioProperties;
+
+    @Bean
+    public MinioClient minioClient() {
+        return MinioClient.builder()
+                .endpoint(minioProperties.getEndpoint())
+                .credentials(minioProperties.getAccessKey(), minioProperties.getSecretKey())
+                .build();
+    }
+}
--- web/web-admin/src/main/java/com/atguigu/lease/web/admin/controller/apartment/FileUploadController.java
+++ web/web-admin/src/main/java/com/atguigu/lease/web/admin/controller/apartment/FileUploadController.java
@@ -2,8 +2,11 @@ package com.atguigu.lease.web.admin.controller.apartment;
 
 
 import com.atguigu.lease.common.result.Result;
+import com.atguigu.lease.web.admin.service.FileService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.checkerframework.checker.units.qual.A;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,11 +18,14 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/admin/file")
 @RestController
 public class FileUploadController {
+    @Autowired
+    private FileService service;
 
     @Operation(summary = "上传文件")
     @PostMapping("upload")
     public Result<String> upload(@RequestParam MultipartFile file) {
-        return Result.ok();
+        String url = service.upload(file);
+        return Result.ok(url);
     }
 
 }
